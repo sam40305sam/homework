57ef1d9eb5b067ef4f6935e8f1d5694ba0bbedf8
sam <sam40305sam@gmail.com>
2018-12-11 22:50:10 +0800

    test

--- config/database.php
+++ config/database.php
@@ -42,11 +42,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '182.50.133.91'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'homework'),
-            'username' => env('DB_USERNAME', 'homework'),
-            'password' => env('DB_PASSWORD', 'tWo2z06!'),
+            'host' => env('DB_HOST', ''),
+            'port' => env('DB_PORT', ''),
+            'database' => env('DB_DATABASE', ''),
+            'username' => env('DB_USERNAME', ''),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',

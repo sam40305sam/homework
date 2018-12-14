83e43db92e13e0f3f6b8e9ff73c863ed742b8a1e
sam <sam40305sam@gmail.com>
2018-12-14 21:08:44 +0800

    a

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ Route::get('/facebook/link', function() {
    
    Route::get('/facebook/callback', function() {
     $user = Socialite::driver('facebook')->user();
-    return var_dump($user);
+    var_dump($user);
    });
 //-*****TEST*****--//
 // Route::get('/test',['as'=>'test','uses'=>
